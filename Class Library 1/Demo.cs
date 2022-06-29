eabbeae6a4fa8a43aa77e58276931b843e86442f
Sandeep-Singh-Gour23 <sandeepsinghgour0@gmail.com>
2022-06-29 19:56:09 +0530

    Initial Commit

---
+++ Class Library 1/Demo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Class_Library_1
+{
+    public class Demo
+    {
+        public void Message()
+        {
+            Console.WriteLine("I am great!");
+        }
+    }
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Class_Library_1;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo d = new Demo();
+            d.Message();
+        }
+    }
+}
